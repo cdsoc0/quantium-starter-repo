33f0cfc30a79a8bdc603673a26093812ca51966a
cdsoc0 <cianocarroll06@gmail.com>
2026-02-09 15:40:55 +0000

    Filtering and some basic styling

--- dash_app.py
+++ dash_app.py
@@ -1,34 +1,56 @@
-import dash as d
+from dash import Dash, dcc, html, Input, Output, callback
 import plotly.express as px
 import pandas as pd
 import csv
 
-app = d.Dash()
+app = Dash()
 sales = []
 dates = []
 regions = []
 
 # Load data
-with open("processed_data.csv" , 'r', newline='') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        sales.append(row["sales"])
-        dates.append(row["date"])
-        regions.append(row["region"])
-
-df = pd.DataFrame({
-    "Sales": sales,
-    "Date": dates,
-    "Region": regions
-})
+df = pd.read_csv("processed_data.csv")
 
 # Setup visuals.
-fig = px.line(df, x="Date", y="Sales", color="Region")
+fig = px.line(df, x="date", y="sales")
 
-app.layout = d.html.Div(children=[
-    d.html.H1(children="Pink Morsel sales over time"),
-    d.dcc.Graph(id="sales-graph", figure=fig)
-])
+app.layout = html.Div(children=[
+    html.H1(children="Pink Morsel sales over time", style={
+        "textAlign": "center",
+    }),
+    html.Div(children=[
+        dcc.Graph(id="sales-graph", figure=fig, style={
+            "flex-grow": "1",
+        }),
+        html.Div(children=[
+            html.H2("Region"),
+            dcc.RadioItems(["all", "north", "east", "south", "west"], id="region-select", value="all")
+        ], style={
+            "flex-grow": "1",
+        })
+    ], style={
+        "display": "flex",
+        "justify-content": "center",
+        "flex-wrap": "wrap",
+        "min-height": "50vh"
+    }),
+], style={
+    "font-family": "sans-serif",
+})
+
+@callback(
+    Output(component_id="sales-graph", component_property="figure"),
+    Input(component_id="region-select", component_property="value")
+)
+def update_visuals(selected_region):
+    if selected_region != "all":
+        filtered_df = df[df.region == selected_region]
+    else:
+        filtered_df = df
+    
+    fig = px.line(filtered_df, x="date", y="sales")
+    fig.update_layout(transition_duration=500)
+    return fig
 
 if __name__ == "__main__":
     app.run(debug=True)
