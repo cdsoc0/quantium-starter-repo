0773415e5cf7a3bd62e1f8fa28a624ed2b6d1e45
cdsoc0 <cianocarroll06@gmail.com>
2026-02-09 14:36:57 +0000

    Data visualisation

---
+++ dash_app.py
@@ -0,0 +1,32 @@
+import dash as d
+import plotly.express as px
+import pandas as pd
+import csv
+
+app = d.Dash()
+sales = []
+dates = []
+regions = []
+
+with open("processed_data.csv" , 'r', newline='') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        sales.append(row["sales"])
+        dates.append(row["date"])
+        regions.append(row["region"])
+
+df = pd.DataFrame({
+    "Sales": sales,
+    "Date": dates,
+    "Region": regions
+})
+
+fig = px.line(df, x="Date", y="Sales", color="Region")
+
+app.layout = d.html.Div(children=[
+    d.html.H1(children="Sales over time"),
+    d.dcc.Graph(id="sales-graph", figure=fig)
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
